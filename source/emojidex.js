0aeb25bc3056ab3abadb19e2394fd29ff357c6bd
uchihamalolan <uchihamalolan@gmail.com>
2019-07-30 17:25:27 +0530

    did all

---
+++ source/emojidex.js
@@ -0,0 +1,7 @@
+const emojidata = require('../assets/emojidata'),
+  emojilist = require('../assets/emojilist');
+
+module.exports = {
+  emojidata,
+  emojilist
+}
---
+++ test/test.js
@@ -0,0 +1,4 @@
+const { emojidata, emojilist } = require('../dist/emojidex');
+
+console.log('emojidata', JSON.stringify(emojidata, null, 2));
+console.log('emojilist', JSON.stringify(emojilist, null, 2));
